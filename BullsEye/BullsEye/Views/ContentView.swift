e2dfb23cc50b381925082762471c191567729911
JillStill <60432918+JillStill@users.noreply.github.com>
2023-05-01 14:22:05 +0300

    Add files via upload

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -15,47 +15,63 @@ struct ContentView: View {
 
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(" 🦎 Put the bullseye as close as you can to")
-                .bold()
-                .kerning(2)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4)
-                .font(.footnote)
-            Text(String(game.target))
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .kerning(-1)
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text(" 🦎 Put the bullseye as close as you can to".uppercased())
+                    .bold()
+                    .kerning(2)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .font(.footnote)
+                    .padding(.horizontal, 30)
+
+                Text(String(game.target))
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .kerning(-1)
+                    
                 
-            
-            HStack{
-                Text("1")
-                    .font(.callout)
-                    .bold()
-                Slider(value: $sliderValue, in: 1...100)
-                Text("100")
-                    .bold()
-                    .font(.callout)
+                HStack{
+                    Text("1")
+                        .font(.callout)
+                        .bold()
+                    Slider(value: $sliderValue, in: 1...100)
+                    Text("100")
+                        .bold()
+                        .font(.callout)
+
+                }
+                .padding()
+                Button(action: {alertIsVisible = true }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20)
+                .background(
+                    ZStack {
+                        Color("HitmeColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    }
+                )
+                .foregroundColor(Color.white)
+                .cornerRadius(21)
+                .alert(isPresented: $alertIsVisible) {
+                    let roundedValue = Int(sliderValue.rounded())
+                  return Alert(
+                    title: Text("Hello there!"),
+                    message: Text("The slider value is \(roundedValue). \nThe total score is \(game.points(sliderValue: roundedValue))"),
+                    dismissButton: .default(Text("Awesome!")))
+                }
 
             }
-            Button(action: {
-                alertIsVisible = true
-            }) {
-              Text("Hit me")
-                
-            }
-            .alert(isPresented: $alertIsVisible) {
-                let roundedValue = Int(sliderValue.rounded())
-              return Alert(
-                title: Text("Hello there!"),
-                message: Text("The slider value is \(roundedValue). \nThe total score is \(game.points(sliderValue: roundedValue))"),
-                dismissButton: .default(Text("Awesome!")))
-            }
-
         }
+        
     }
 }
 
